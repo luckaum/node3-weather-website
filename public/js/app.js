0551a55e31f98fca051c4a3005fedef0d098f6b9
Lucas <lucasafeng@gmail.com>
2020-12-01 17:12:53 -0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?local=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.message) {
+            messageOne.textContent = data.message
+        }else {
+            messageOne.textContent = data.cityName
+            messageTwo.textContent = data.temperature
+        }
+    })
+})
+})
---
+++ src/app.js
@@ -0,0 +1,100 @@
+//Core modules
+const path = require('path')
+
+//npm modules
+const express = require('express')
+const hbs = require('hbs')
+
+//Custom modules
+const openWeather = require('../../weather-app/utils/openWeather')
+
+
+const app = express()
+
+//Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Lucas'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Lucas'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Help text',
+        title: 'Help',
+        name: 'Lucas'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.local) {
+        return res.send({
+            error: 'A local must be provided!'
+        })
+    }
+    const localWeather = req.query.local
+    openWeather(localWeather, (err, data) => {
+        if (err === undefined) {
+            return res.send({
+                cityName: data.body.name,
+                temperature: data.body.main.temp
+            })
+        }
+        res.send({message: err})
+        
+    })
+})
+
+//Example use
+
+// app.get('/products', (req, res) => {
+//     if(!req.query.search) {
+//         return res.send({
+//             error: 'You must provide a search term'
+//         })
+//     }
+//     console.log(req.query.search)
+//     res.send({
+//         products: []
+//     })
+// })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Lucas',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Lucas',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
